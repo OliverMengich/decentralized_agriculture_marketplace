9ea979b6f8519bcd912df6bcdc3fb1e327a23e7f
OliverMengich <kemeioliver2000@gmail.com>
2022-03-17 10:29:07 +0300

    solved the transfer from function. Glory to God

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -10,67 +10,59 @@ contract ERC721{
         string _dateOfPlant;
         string _harvestDate;
         address _owner;
+        uint count;
     }
-    enum State {NO_PURCHASE, NEW_ORDER, AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE }
 
-    struct OrdersStruct{
-        Products agricultureProducts;
-        address buyer;
-        State _orderstate;
-    }
-    OrdersStruct[] private orders;
-    mapping (address=>OrdersStruct[]) private _customerorders;
-    //mapping(address => mapping (address=>OrdersStruct[])) public _buyerorders;
-
-    event Transfer(address indexed _from, address indexed _to, Products product_Transferred);
     Products[] public product;
     mapping (address=>Products[]) internal ownerProducts;
     mapping(address => bool) public isMember;
 
-    
-    
-    
-
-
-
-    function transferFrom(address _from, address payable _to, uint256 _index) public payable returns(string memory) {
+    function transferFrom(address _from, address payable _to, uint256 _index) public payable returns(bool) {
         //require(_from.balance > price,'You have insufficient balance to transact');
         require(_from != address(0),'Invalid address');
         require(_to != address(0),' Invalid address');
+        require(address(_to) != address(_from) ,'you cannot send to yourself');
         require(isMember[_from] == true,'not a member');
         
-        string memory issuccessfull = _transferFrom(_from, _to, _index);
-        return issuccessfull;
-    }
-    function getElement(uint256 _index,address _owner) private view returns(uint256){
-         Products[] memory alluserproducts = ownerProducts[_owner];
-        for(uint i=0; i < alluserproducts.length; i++){
-            bytes32 hash1 = keccak256(abi.encodePacked(alluserproducts[i].product_name,address(alluserproducts[i]._owner)));
-            bytes32 hash2 = keccak256(abi.encodePacked(product[_index].product_name,address(product[_index]._owner)));
-            if(hash1 == hash2) return i;
-        }
-        return 455353535635635635;
-    }
-    function _transferFrom(address _from, address payable _to, uint256 _index) private returns(string memory){
-        Products memory specificProduct = ownerProducts[_from][_index];
-        //require if the _from has the product if not revert
-        require(specificProduct.price != 0,'no product found');
-        require(specificProduct.product_quantity != 0,'product quantity not specified');
-        // delete that product from the person who owns it
-        uint indexInArray = getElement(_index, _from);
-        require(indexInArray != 455353535635635635,'element not found');
-        deleteFromUser(_from, indexInArray);
-        // change ownership from current owner to latest owner
-        specificProduct._owner = _to;
-        //change from records 
-        product[_index]._owner = _to;
-        ownerProducts[_to].push(specificProduct);
-        return 'Transfer successfull';
+        bool result = _transferFrom(_from, _to, _index);
+        return result;
     }
     
-    function BuyCommodity(address payable _to, uint value) public payable returns(bool, bytes memory){
-        (bool success, bytes memory data) = _to.call{value: value}("");
-        return (success, data);
+    function _transferFrom(address _from, address payable _to, uint256 _index) private returns(bool){
+        
+        // 1. find product from products array
+        Products memory specificProduct = product[_index];
+        require(specificProduct.price != 0,'no product found');
+        require(specificProduct.product_quantity != 0,'product quantity not specified');
+        require(address(specificProduct._owner) == address(_from),' you dont own this product');
+        //2. find the product count 
+        uint _productCount = specificProduct.count;
+        //3. delete product from ownerProducts
+        deleteFromUser( _from, _productCount);
+        //4. change product ownership from _from to _to
+        specificProduct._owner = address(_to);
+        //5. delete from overall array and update the overall array
+        deleteFromOverallArray(_index);
+        product.push(specificProduct);
+        //5. put _to as member
+        isMember[_to] = true;
+        //6. update the buyers struct
+        ownerProducts[_to].push(specificProduct);
+        return true;
+    }
+    function deleteFromUser(address _from, uint _productCount) private {
+        Products[] storage owninguser = ownerProducts[_from];
+
+        for(uint i = _productCount; i< owninguser.length-1; i++){
+            owninguser[i] = owninguser[i+1]; 
+        }
+        owninguser.pop();
+    }
+    function deleteFromOverallArray(uint _index) private{
+        for(uint i = _index; i< product.length-1; i++){
+            product[i] = product[i+1]; 
+        }
+        product.pop();
     }
     function totalSupply() public virtual view returns(uint256){
         return product.length;
@@ -84,21 +76,7 @@ contract ERC721{
     function tokenByIndex(uint256 _index) public virtual view returns(Products memory){
         
         return product[_index];
-    }
-    
-    function deleteFromUser(address _from, uint256 _index) private {
-        Products[] storage owninguser = ownerProducts[_from];
-        require(_index < owninguser.length, 'index out of bounds');
-        for(uint i = _index; i< owninguser.length-1; i++){
-            owninguser[i] = owninguser[i+1]; 
-        }
-        owninguser.pop();
-    }
-    function testArrayProduct(uint256 _index) public returns(Products memory){
-        Products storage myprod = product[_index];
-        myprod._owner = 0x1c9264Bd829884899dc15397BeC23113bCeC3982;
-        return myprod;
-    }
+    } 
     //returns balance of an owner
     function balanceOf(address _owner) public view returns(Products[] memory) {
         require(_owner != address(0));
@@ -112,52 +90,17 @@ contract ERC721{
             price: price,
             _dateOfPlant: _dateOfPlant,
             _harvestDate: _harvestDate,
-            _owner: _owner
+            _owner: _owner,
+            count: product.length + 1
         });
         product.push(prod);
         //call the mint function  
         addProduct(_owner, prod);   
     }
     function addProduct(address _owner, Products memory prod) private{
-        ownerProducts[_owner].push(prod);
+        ownerProducts[_owner].pushprod);
         isMember[_owner] = true;
     }
-    function AddOrder(address _buyer, uint256 _index, address _owner) public {
-       Products memory prod = ownerProducts[_owner][_index];
-        OrdersStruct memory neworder = OrdersStruct({
-            agricultureProducts: prod,
-            _orderstate: State.NEW_ORDER,
-            buyer: _buyer
-        });
-        orders.push(neworder);
-        //_buyerorders[_buyer] = _customerorders[_owner].push(neworder);
-    }
-    function AcceptOrder( address _seller, uint _index) public view{
-        OrdersStruct memory order = _customerorders[_seller][_index];
-        require(order._orderstate == State.NEW_ORDER);
-        order._orderstate = State.AWAITING_PAYMENT;
-
-        // notify user in the frontend that order is accepted
-    }
-    function check_orders(address _buyer) public view returns(OrdersStruct[] memory) {
-        return _customerorders[_buyer];
-    }
-    function ProceedCheckOut( uint _index, address payable _seller, address _buyer) public {
-        OrdersStruct memory order = _customerorders[_seller][_index];
-        require(order._orderstate == State.AWAITING_PAYMENT);
-        BuyCommodity(_seller,order.agricultureProducts.price);
-
-        //delete from ownerProducts array
-        transferFrom(_buyer, _seller, _index);
-        order._orderstate = State.AWAITING_DELIVERY;
-        //delete in products array
-        deleteFromProductsArray(_index);
-    }
-    function deleteFromProductsArray(uint index) private{
-        for(uint i = index; i< product.length-1; i++){
-            product[i] = product[i+1]; 
-        }
-        product.pop();
-    }
+    
     
 }
